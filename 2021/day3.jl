daeef05b4d573e8ae92494ebfff6cecf0af22e7a
Darren Chang <darren.ch12@gmail.com>
2021-12-06 00:24:18 -0500

    Update day 3

--- 2021/day3.jl
+++ 2021/day3.jl
@@ -3,61 +3,36 @@ using DataFrames, DelimitedFiles
 input_path = joinpath(dirname(dirname(pwd())), "aoc_input")
 f = readlines(joinpath(input_path, "2021/day3.txt"))
 
-A = zeros(length(f), length(f[1]))
-for i in 1:length(f)
-    for j in 1:length(f[1])
-        A[i,j] = parse(Int, f[i][j])
-    end
-end
+gen_matrix(f) = permutedims(reshape([parse(Int, f[i][j]) for i=1:length(f) for j=1:length(f[1])], 
+(length(f[1]), length(f))))
+convert_to_dec(v) = parse(Int, join(Int.(v)), base = 2)
+
+A = B = gen_matrix(f)
 
 # part 1
 res = sum(A, dims = 1)/size(A, 1) .>= 0.5
+convert_to_dec(res)*convert_to_dec(.!res) # 775304
 
-convert_to_dec(v) = parse(Int, join(Int.(v)), base = 2)
-γ = convert_to_dec(res)
-ϵ = convert_to_dec(.!res)
-γ*ϵ # 775304
+# part 2
+find_most(B, c) = sum(B[1:end, c])/size(B)[1] >= 0.5
 
-# # part 2
-# j = 1
-# A[1:end, 1]
+keep_most = function(B, d)
+    if size(B,1) > 1
+        return B[B[1:end,d] .== Int(find_most(B, d)), 1:end]
+    else return B
+    end
+end
 
+keep_least = function(B, d)
+    if size(B,1) > 1
+        return B[B[1:end,d] .== Int(!find_most(B, d)), 1:end]
+    else return B
+    end
+end
 
-# # will figure this out later
-# find_most(A, c) = Int(sum(A[1:end, c])/size(A)[1] >= 0.5)
+for j in 1:length(f[1])
+    A = keep_most(A, j)
+    B = keep_least(B, j)
+end
 
-# keep_most = function(A, c)
-#     if c <= size(A,2)
-#         z = find_most(A, c)
-#         c += 1
-#         return (A[A[1:end,c] .== z,1, 1:end], c)
-#     end
-# end
-
-# keep_most(keep_most(A, 1))
-
-# # part 2
-# calc_o2 = function(f)
-#     o = f
-#     j = 1
-#     find_idx = function(j, o)
-#         a = 0
-#         idx = []
-#         for i in 1:length(o)
-#             # for j in 1:length(f[1])
-#             a += parse(Int, o[i][j])
-#             if o[i][j] == string(Int(a/length(o)>=0.5))
-#                 push!(idx, i)
-#             end
-#         end
-#         j += 1
-#         o = o[idx]
-#         return idx
-#     end
-#     while length(o) > 1
-#         j, o = find_idx(j, o)
-#     end
-#     return(o)
-# end
-
-# calc_o2(f)
+convert_to_dec(A)*convert_to_dec(B) # 1370737
